a975adb9ef082f2e21f9bbbedc122fb6f98bcaf5
Egor <popa256666@icloud.com>
2024-11-06 15:42:03 +0300

    Добавьте файлы проекта.

---
+++ реализация хеш-таблицы/main.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <memory>
+#include <cstdlib>
+
+#define CAPACITY 5000;
+
+using std::endl;
+using std::cout;
+
+struct Ht_item {
+    char* key;
+    char* value;
+};
+
+struct HashTable {
+    Ht_item** items;
+    int size;
+    int count;
+};
+
+unsigned long hash_function(char* str) {
+    unsigned long i = 0;
+    for (int j = 0; str[j]; j++)
+        i += str[j];
+    return i % CAPACITY;
+}
+
+Ht_item* create_item(char* key, char* value) {
+    Ht_item* item = (Ht_item*)malloc(sizeof(Ht_item));
+    item->key = (char*)malloc(strlen(key) + 1);
+    item->value = (char*)malloc(strlen(value) + 1);
+
+    strcpy(item->key, key);
+    strcpy(item->value, value);
+
+    return item;
+}
+
+HashTable* create_table(int size) {
+    // Creates a new HashTable
+    HashTable* table = (HashTable*)malloc(sizeof(HashTable));
+    table->size = size;
+    table->count = 0;
+    table->items = (Ht_item**)calloc(table->size, sizeof(Ht_item*));
+    for (int i = 0; i < table->size; i++)
+        table->items[i] = NULL;
+    return table;
+}
+
+void free_item(Ht_item* item) {
+    // Frees an item
+    free(item->key);
+    free(item->value);
+    free(item);
+}
+
+void free_table(HashTable* table) {
+    // Frees the table
+    for (int i = 0; i < table->size; i++) {
+        Ht_item* item = table->items[i];
+        if (item != NULL)
+            free_item(item);
+    }
+
+    free(table->items);
+    free(table);
+}
+
+void handle_collision(HashTable* table, Ht_item* item) {
+}
+
+void ht_insert(HashTable* table, char* key, char* value) {
+    // Create the item
+    Ht_item* item = create_item(key, value);
+
+    unsigned index = hash_function(key);
+
+    Ht_item* current_item = table->items[index];
+
+    if (current_item == NULL) {
+        // Key does not exist.
+        if (table->count == table->size) {
+            // Hash Table Full
+            printf("Insert Error: Hash Table is full\n");
+            return;
+        }
+
+
+        // Insert directly
+        table->items[index] = item;
+        table->count++;
+    }
+
+    else {
+        // Scenario 1: We only need to update value
+        if (strcmp(current_item->key, key) == 0) {
+            strcpy(table->items[index]->value, value);
+            return;
+        }
+
+        else {
+            // Scenario 2: Collision
+             // We will handle case this a bit later
+            handle_collision(table, item);
+            return;
+        }
+    }
+}
+
+char* ht_search(HashTable* table, char* key) {
+    // Searches the key in the hashtable
+    // and returns NULL if it doesn't exist
+    int index = hash_function(key);
+    Ht_item* item = table->items[index];
+
+    // Ensure that we move to a non NULL item
+    if (item != NULL) {
+        if (strcmp(item->key, key) == 0)
+            return item->value;
+    }
+    return NULL;
+}
+
+int main()
+{
+	
+	return 0;
+}
